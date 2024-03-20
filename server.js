fe55f3155193f776f4964211558f5bdd162839af
Shaun <shaun@Kellys-MBP.hsd1.co.comcast.net>
2024-03-20 15:42:32 -0600

    updated operations

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 const pg = require('pg')
 const client = new pg.Client(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_notes_categories_db'
+  process.env.DATABASE_URL || 'postgres://localhost/employees'
 )
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(require('morgan')('dev'))
 
-app.get('/api/categories', async (req, res, next) => {
+app.get('/api/employee', async (req, res, next) => {
   try {
     const SQL = `
-      SELECT * from categories
+      SELECT * from employee
     `
     const response = await client.query(SQL)
     res.send(response.rows)
@@ -21,10 +21,10 @@ app.get('/api/categories', async (req, res, next) => {
   }
 })
 
-app.get('/api/notes', async (req, res, next) => {
+app.get('/api/department', async (req, res, next) => {
   try {
     const SQL = `
-      SELECT * from notes ORDER BY created_at DESC;
+      SELECT * from department ORDER BY created_at DESC;
     `
     const response = await client.query(SQL)
     res.send(response.rows)
@@ -33,11 +33,11 @@ app.get('/api/notes', async (req, res, next) => {
   }
 })
 
-app.post('/api/notes', async (req, res, next) => {
+app.post('/api/department', async (req, res, next) => {
   try {
     const SQL = `
-      INSERT INTO notes(txt, category_id)
-      VALUES($1, $2)
+      INSERT INTO department(name, department_id)
+      VALUES($"Grocery", $2)
       RETURNING *
     `
     const response = await client.query(SQL, [req.body.txt, req.body.category_id])
@@ -47,12 +47,12 @@ app.post('/api/notes', async (req, res, next) => {
   }
 })
 
-app.put('/api/notes/:id', async (req, res, next) => {
+app.put('/api/department/:id', async (req, res, next) => {
   try {
     const SQL = `
-      UPDATE notes
-      SET txt=$1, ranking=$2, category_id=$3, updated_at= now()
-      WHERE id=$4 RETURNING *
+      UPDATE department
+      SET name=$Fitness
+      WHERE id=$3 RETURNING *
     `
     const response = await client.query(SQL, [
       req.body.txt,
@@ -66,10 +66,10 @@ app.put('/api/notes/:id', async (req, res, next) => {
   }
 })
 
-app.delete('/api/notes/:id', async (req, res, next) => {
+app.delete('/api/department/:id', async (req, res, next) => {
   try {
     const SQL = `
-      DELETE from notes
+      DELETE from department
       WHERE id = $1
     `
     const response = await client.query(SQL, [req.params.id])
@@ -82,33 +82,30 @@ app.delete('/api/notes/:id', async (req, res, next) => {
 const init = async () => {
   await client.connect()
   let SQL = `
-    DROP TABLE IF EXISTS notes;
-    DROP TABLE IF EXISTS categories;
-    CREATE TABLE categories(
+    DROP TABLE IF EXISTS employee;
+    DROP TABLE IF EXISTS department;
+
+    CREATE TABLE employee(
       id SERIAL PRIMARY KEY,
       name VARCHAR(100)
     );
-    CREATE TABLE notes(
+    CREATE TABLE department(
       id SERIAL PRIMARY KEY,
+      name VARCHAR(100),
       created_at TIMESTAMP DEFAULT now(),
       updated_at TIMESTAMP DEFAULT now(),
-      ranking INTEGER DEFAULT 3 NOT NULL,
-      txt VARCHAR(255) NOT NULL,
-      category_id INTEGER REFERENCES categories(id) NOT NULL
+      department_id INTEGER REFERENCES department(id)
     );
   `
   await client.query(SQL)
   console.log('tables created')
   SQL = `
-    INSERT INTO categories(name) VALUES('SQL');
-    INSERT INTO categories(name) VALUES('Express');
-    INSERT INTO categories(name) VALUES('Shopping');
-    INSERT INTO notes(txt, ranking, category_id) VALUES('learn express', 5, (SELECT id FROM categories WHERE name='Express'));
-    INSERT INTO notes(txt, ranking, category_id) VALUES('add logging middleware', 5, (SELECT id FROM categories WHERE name='Express'));
-    INSERT INTO notes(txt, ranking, category_id) VALUES('write SQL queries', 4, (SELECT id FROM categories WHERE name='SQL'));
-    INSERT INTO notes(txt, ranking, category_id) VALUES('learn about foreign keys', 4, (SELECT id FROM categories WHERE name='SQL'));
-    INSERT INTO notes(txt, ranking, category_id) VALUES('buy a quart of milk', 2, (SELECT id FROM categories WHERE name='Shopping'));
-  `
+    INSERT INTO employee(name) VALUES('Shaun'), ('Kelly'), ('Charlie');
+    INSERT INTO department(name, department_id) VALUES
+      ('Grocery',  (SELECT id FROM employee WHERE name='Shaun')),
+      ('Fitness',  (SELECT id FROM employee WHERE name='Shaun')),
+      ('Supplies', (SELECT id FROM employee WHERE name='Kelly'));
+  `;
   await client.query(SQL)
   console.log('data seeded')
   app.listen(port, () => console.log(`listening on port ${port}`))
